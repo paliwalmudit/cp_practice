07e32187bb94825d265d9b850317457c963124cd
paliwalmudit <muditpaliwal2099@gmail.com>
2023-08-18 16:24:25 +0530

    stack(great_string)

---
+++ make_the_string_great.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def makeGood(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        stack=deque()
+        for i in s:
+            if stack and abs(ord(stack[-1])-ord(i))==32:
+                stack.pop()
+            else:stack.append(i)
+
+        return ''.join(stack)
